47c61a8158c509c84b6de2bb066080f0b73f5edf
arslansherazi <arslanhaidersherazi@hotmail.com>
2020-09-28 19:28:38 +0500

    login screen

---
+++ src/screens/login/component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Template } from './template.js'
+
+
+export default class Login extends Component
+{
+    render() 
+    {
+        return <Template />
+    }
+}
---
+++ src/screens/login/template.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './styling.css'
+
+
+export class Template extends Component
+{
+    render ()
+    {
+        return(
+            <div>
+                <h2>Login</h2>
+            </div>
+        )
+    }
+}
